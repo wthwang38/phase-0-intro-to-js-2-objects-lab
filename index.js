2284535e0f508c4a2b795fe43c9fdaf87e29c21f
William Hwang <willhwang90@gmail.com>
2022-05-02 10:12:25 -0400

    commit tree 2d2f34979740ad6ce907e311280243d052ea0d2e

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "William",
+    address: "flushing"
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+   const newEmployee = {...employee}
+
+   newEmployee[key] = value;
+
+   return newEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value)=>{
+     employee[key] = value;
+
+     return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    
+    const newEmployee = {...employee};
+    
+    delete newEmployee[key];
+    
+    return newEmployee;    
+}
+
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+
+    return employee;
+}
